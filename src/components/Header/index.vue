05d07b02f2047cdc860c69c6369f2884e9f7e19c
huangsheng912 <631405221@qq.com>
2022-02-21 18:58:12 +0800

    网络错误图标调整

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -25,7 +25,7 @@
               <SwitchChain v-model="showSwitchChain" :chainId="chainId">
                 <div class="l1-chain" @click="showSwitchChain = true">
                   <img :src="chainLogo" alt="" v-if="!wrongChain" />
-                  <img src="../../assets/img/nerveIcon.png" alt="" @click="showSwitchChain = true" v-else />
+                  <img src="../../assets/img/net-error.jpg" alt="" @click="showSwitchChain = true" v-else />
                   丨
                 </div>
               </SwitchChain>
