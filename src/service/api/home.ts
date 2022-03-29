97e7e0b9609a625877d7a953fb9f24d492b2b3f2
huangsheng912 <631405221@qq.com>
2022-03-29 15:02:18 +0800

    首页添加统计数据

--- src/service/api/home.ts
+++ src/service/api/home.ts
@@ -1,7 +1,31 @@
 import http from '@/service';
+import { genId } from '@/utils/util';
+import { listen } from '@/service/socket/promiseSocket';
+import config from '@/config';
+
+const url = config.WS_URL;
 
 // 我的节点信息
 export async function getOverviewData() {
   const res = await http.rPost('assetGet');
   return res?.result;
+}
+
+// 获取汇总信息
+export async function getSummaryData() {
+  const channel = 'db_summary_info';
+  const params = {
+    method: channel,
+    id: genId(),
+    params: {}
+  };
+  return await listen({
+    url,
+    channel,
+    id: params.id,
+    params: {
+      cmd: true,
+      channel: 'cmd:' + JSON.stringify(params)
+    }
+  });
 }
